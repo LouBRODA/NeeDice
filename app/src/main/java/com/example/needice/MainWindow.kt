f9718eb34980b10e79535ec30bf7c137b26a4b59
lou.broda <lou.broda@etu.uca.fr>
2023-03-05 15:36:15 +0100

    add mainActivity + playscreen :zap:

---
+++ app/src/main/java/com/example/needice/MainWindow.kt
@@ -0,0 +1,12 @@
+package com.example.needice
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainWindow : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
