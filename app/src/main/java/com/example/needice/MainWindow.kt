888c681f72362a6cce7eff0cbaeba5b5ab79caf1
lou.broda <lou.broda@etu.uca.fr>
2023-03-08 16:46:56 +0100

    add first version startable with 2 drawer layout (left & right)

--- app/src/main/java/com/example/needice/MainWindow.kt
+++ app/src/main/java/com/example/needice/MainWindow.kt
@@ -1,12 +1,13 @@
 package com.example.needice
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainWindow : Activity() {
+class MainWindow : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.playscreen)
     }
 
 }
