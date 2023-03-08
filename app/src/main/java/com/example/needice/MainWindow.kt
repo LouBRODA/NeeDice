f958b80118ac1c7d54d7cc14fe89dd1c3b780315
lou.broda <lou.broda@etu.uca.fr>
2023-03-08 18:54:51 +0100

    nice home page :art: (nav ToDo)

--- app/src/main/java/com/example/needice/MainWindow.kt
+++ app/src/main/java/com/example/needice/MainWindow.kt
@@ -1,13 +1,14 @@
 package com.example.needice
 
 import android.os.Bundle
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 
 class MainWindow : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.playscreen)
+        setContentView(R.layout.home_screen)
     }
 
 }
