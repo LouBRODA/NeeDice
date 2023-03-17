84bcab8c8094fb538d3b279a82e40dabd73d9965
lou.broda <lou.broda@etu.uca.fr>
2023-03-17 17:36:42 +0100

    text to speech OK :rocket:

--- app/src/main/java/but/app/needice/view/MainWindow.kt
+++ app/src/main/java/but/app/needice/view/MainWindow.kt
@@ -1,7 +1,12 @@
 package but.app.needice.view
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.util.Log
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -11,15 +16,38 @@ import androidx.recyclerview.widget.RecyclerView
 import but.app.needice.R
 import but.app.needice.adaptor.ColorPalletAdaptor
 import but.app.needice.model.Color
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.random.Random
 
 
-class MainWindow : AppCompatActivity() {
+class MainWindow : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    var listen : TextToSpeech? = null
+    lateinit var roll : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.play_screen)
+        roll = findViewById(R.id.roll_button)
+        listen = TextToSpeech(this, this)
         rollDice()
+    }
+
+    override fun onInit(state: Int) {
+        if (state == TextToSpeech.SUCCESS) {
+            val result = listen!!.setLanguage(Locale.FRENCH)
+
+            /*if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TextToSpeak","ERR : Language not supported !")
+            }else{
+                roll.isEnabled = false
+            }*/
+        }
+    }
+
+    private fun speakOut(text : String) {
+        listen!!.speak(text, TextToSpeech.QUEUE_FLUSH, null)
     }
 
     override fun onResume() {
@@ -31,9 +59,9 @@ class MainWindow : AppCompatActivity() {
     }
 
     private fun rollDice(){
-        val roll : Button = findViewById(R.id.roll_button)
         val text : TextView = findViewById(R.id.number)
         val rd : Random = Random
+
         roll.setOnClickListener {
             val de = 1 + rd.nextInt(7 - 1)
             text.text = de.toString()
@@ -42,7 +70,10 @@ class MainWindow : AppCompatActivity() {
             val form : View = findViewById(R.id.dice_form);
             val animation = AnimationUtils.loadAnimation(this, R.anim.dice_animation)
             form.startAnimation(animation)
+            speakOut(text.text.toString())
         }
     }
+
+
 
 }
